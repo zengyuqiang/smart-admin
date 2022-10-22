2621703f1f04984fcc719939ff01c5836c71978e
zhuoda <zhuoluodada@qq.com>
2022-10-22 20:49:25 +0800

    2.0的js版本和后端 完成

---
+++ smart-admin-web/javascript-ant-design-vue3/src/lib/smart-sentry.js
@@ -0,0 +1,12 @@
+export const smartSentry = {
+  /**
+   * sentry 主动上报
+   */
+  captureError: (error) => {
+    if (error.config && error.data && error && error.headers && error.request && error.status) {
+      return;
+    }
+    // Sentry.captureException(error);
+    console.error(error);
+  },
+};
